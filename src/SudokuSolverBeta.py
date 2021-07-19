5ee7047af345ef6cb28b76f65718020bfc81753a
MikelGV <mikelgaldosv@gmail.com>
2021-07-19 23:31:07 +0200

    sudoku solver in commandline completed

--- src/SudokuSolverBeta.py
+++ src/SudokuSolverBeta.py
@@ -1,17 +1,9 @@
-grid = [[0,0,0,8,0,1,0,0,0],
-        [0,0,0,0,7,0,8,0,0],
-        [0,0,0,0,0,0,1,0,0],
-        [0,2,0,0,3,0,0,0,0],
-        [6,0,0,0,0,0,0,7,5],
-        [0,0,3,4,0,0,0,0,0],
-        [0,0,0,2,0,0,6,0,0],]
-
 def find_next_empty(puzzle):
-    for r in range(9):
-        for c in range(9):
-            if puzzle[r][c] == -1:
-                return r,c
-    return None, None
+	for r in range(9):
+		for c in range(9):
+			if puzzle[r][c] == -1:
+				return r,c
+	return None, None
 
 def is_valid(puzzle, guess, row, col):
 	
@@ -33,11 +25,31 @@ def is_valid(puzzle, guess, row, col):
 	return True
 
 def solve_sudoku(puzzle):
-    row, col = find_next_empty(puzzle)
-    if row is None:
-        return True
-	for guess in range(1, 10):
+	row, col = find_next_empty(puzzle)
+	if row is None:
+		return True
+	for guess in range(1,10):
 		if is_valid(puzzle, guess, row, col):
-			puzzle[row][col] == guess
-		if solve_sudoku(puzzle):
-			return True
+			puzzle[row][col] = guess
+			if solve_sudoku(puzzle):
+				return True
+		puzzle[row][col] = -1
+
+
+if __name__ == '__main__':
+	example_board = [
+
+		[3,9,-1, -1,5,-1, -1,-1, 1],
+		[-1,-1,-1, 2,-1,-1, -1,-1,5],
+		[-1,-1,-1, 7,1,9, -1,8,-1],
+
+		[-1,5,-1, -1,6,8, -1,-1,-1],
+		[2,-1,6, -1,-1,3, -1,-1,-1],
+		[-1,-1,-1, -1,-1,-1, -1,-1,4],
+
+		[5,-1,-1, -1,-1,-1, -1,-1,-1],
+		[6,7,-1, 1,-1,5, -1,4,-1],
+		[1,-1,9, -1,-1,-1, 2,-1,-1]
+	]
+	print(solve_sudoku(example_board))
+	print(example_board)
